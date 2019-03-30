d3ee21457f10f38da586bfbc1b4646fd34fccaa9
mgiwsk <46477907+mgiwsk@users.noreply.github.com>
2019-03-29 21:52:24 -0700

    Add files via upload

---
+++ lab3/app/disappear.js
@@ -0,0 +1,11 @@
+AFRAME.registerComponent('disappear', {    
+
+   init: function () {         
+
+      let disappearfunc = () => {      
+      this.el.setAttribute("visible", false);    
+      };        
+
+      this.el.addEventListener('click', disappearfunc);      
+      }   
+});
